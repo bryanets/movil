cc97034ad6effb1d69d558b535a0830bbd14ab93
bryanets <bryantrebejo@hotmail.com>
2023-12-03 06:29:29 -0500

    login v1.4

--- src/main/java/com/cibertec/service/UserServiceImpl.java
+++ src/main/java/com/cibertec/service/UserServiceImpl.java
@@ -10,37 +10,39 @@ import com.cibertec.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-    
+
     @Autowired
     private UserRepository repository;
-    
+
     @Override
     public User registrar(User user) {
-        
-        if (repository.findByNumCard(user.getNumCard()) == null || repository.findByDni(user.getDni()) == null) {
-            user = null;
-        } else {
-            user = repository.save(user);
+
+        if (repository.findByNumCard(user.getNumCard()) == null) {
+            if (repository.findByDni(user.getDni()) == null) {
+                user = repository.save(user);
+            } else {
+                user = null;
+            }
         }
-        
+
         return user;
     }
-    
+
     @Override
     public User login(String dni, String pass, String numCard) {
         return repository.login(dni, pass, numCard);
     }
-    
+
     @Override
     public void delete(String id) {
         repository.deleteById(id);
     }
-    
+
     @Override
     public List<User> list() {
         return repository.findAll();
     }
-    
+
     @Override
     public User update(User user) {
         return repository.save(user); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
